05e59c533f16180552fe351282720c8c13eda43b
M <m1@mishe1.ru>
2026-02-02 00:04:54 +0300

    4.1-3 Добавлены тесты для GetSize

---
+++ path_size_test.go
@@ -0,0 +1,42 @@
+package code
+
+import "testing"
+
+func TestGetSize_File2b(t *testing.T) {
+	path := "testdata/file_2b.txt"
+	got, err := GetSize(path)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	want := "2\t" + path
+	if got != want {
+		t.Fatalf("want: %q\ngot:  %q", want, got)
+	}
+}
+
+func TestGetSize_DirWithOneFile(t *testing.T) {
+	path := "testdata/dir_with_one_file"
+	got, err := GetSize(path)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	want := "8\t" + path
+	if got != want {
+		t.Fatalf("want: %q\ngot:  %q", want, got)
+	}
+}
+
+func TestGetSize_EmptyDir(t *testing.T) {
+	path := "testdata/empty_dir"
+	got, err := GetSize(path)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	want := "0\t" + path
+	if got != want {
+		t.Fatalf("want %q, got %q", want, got)
+	}
+}
